d4865a3722fa08ff61fcc9baf331e57785ba2813
salehkaddoura <saleh.kaddoura@gmail.com>
2015-07-09 00:13:21 -0700

    removing config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-var config;
-config = module.exports;
-
-process.env.ACCESS_KEY = 'AKIAIPLMO3URB62ICYEA';
-process.env.SECRET_KEY = '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';
-process.env.REGION = 'us-west-1';
